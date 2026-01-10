71592d40a38ab31ebc7ce914a040e8dcd9d71502
Mikhail Fomichev <fomichevmi@gmail.com>
2026-01-10 14:32:45 +0100

    Init commit

---
+++ src/main/java/com/mif/interview/yapily/YapilyApplication.java
@@ -0,0 +1,14 @@
+package com.mif.interview.yapily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YapilyApplication.class, args);
+		System.out.println("I'm alive");
+	}
+
+}
---
+++ src/test/java/com/mif/interview/yapily/YapilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mif.interview.yapily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
